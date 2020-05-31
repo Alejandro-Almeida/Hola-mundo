4389fc30815ecd7046bda40107c7766378a9eff7
HolgerCatucuamba <63126510+HolgerCatucuamba@users.noreply.github.com>
2020-05-31 16:18:18 -0500

    Version1.1_A1.Almeida.René.POO.cpp

--- A1.Almeida.René.ProgramacionOrientadaObjetos.cpp
+++ A1.Almeida.René.ProgramacionOrientadaObjetos.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    float mensaje;
+int main(){
+    string mensaje;
     cout<<"Hola Mundo"<<endl;
+    cout<<"Ingrese un mensaje: "<<endl;
     cin>>mensaje;
+    cout<<"Su mensaje es: "<<mensaje<<endl;
     system("PAUSE");
     return 0;
 }
